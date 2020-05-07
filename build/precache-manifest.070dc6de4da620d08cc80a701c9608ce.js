52a66243db7399f41b19829b3e55225f7c218170
Jonna <jc2627@cornell.edu>
2020-05-07 02:24:06 -0400

    deployed onto firebase!

---
+++ build/precache-manifest.070dc6de4da620d08cc80a701c9608ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8a16dfce856dfc8b8ca9f7fcfaf654",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cacfbb0489c82ca8b572",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "eaa559be248e2881126f",
+    "url": "/static/js/2.c3fe46da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c3fe46da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cacfbb0489c82ca8b572",
+    "url": "/static/js/main.0f56ff10.chunk.js"
+  },
+  {
+    "revision": "ca3da31c2873b2c47157",
+    "url": "/static/js/runtime-main.43a5998a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

ed2d268cf348f603dea1d0764aa22e42036ef838
Jonna <jc2627@cornell.edu>
2020-05-07 23:43:03 -0400

    save all to repo

---
+++ src/App.js
@@ -0,0 +1,219 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div className="App" >
+
+      <header>
+
+      <h1><a target = "_blank" href= "https://www.thisismyspacesuit.com/">THIS IS MY SPACESUIT.</a></h1>
+
+      <h2><a target = "_blank" href = "https://shoshanaswell.com/"><strong>Shoshana Swell</strong></a> on creativity and passion projects in and out of college and quarantine</h2>
+
+      </header>
+
+      <div id = "intro">
+
+      <p> From juggling a diverse course load to launching her own fashion brand, <a target = "_blank" href = "https://shoshanaswell.com/"><strong>Shoshana Swell</strong></a> has redefined the meaning of “blasting off”. Currently studying Information Science and Performing and Media Arts at Cornell University, she started <a target = "_blank" href = "https://www.thisismyspacesuit.com/">THIS IS MY SPACESUIT</a> in her junior year in response to NASA cancelling their all female-space mission due to a lack of female-fitting spacesuits. Since that May of 2019, the company has hosted educational workshops in Ithaca, Ghana, Eswatini, and Milan, and was even invited by NASA to the Kennedy Space Center in July 2019. In a Zoom-facilitated interview, we talked about her journey in creating this brand from scratch, how she’s been managing to stay creative during in COVID-19 quarantine, and advice she has for us everyday astronauts.
+      </p>
+
+      <p id = "credit"><em>Article written and website coded by Jonna Chen</em></p>
+
+      </div>
+
+      <div className = "panel" id = "section1">
+
+      <div className = "blur">
+
+      <img src = "images/question1.png" alt = "portrait photo of Shoshana Swell smiling" />
+
+      </div>
+
+      <div className = "question question-1">
+
+      <p>JC: Tell me more about the details of how you started right after being moved by the NASA story. You launched the first collection in just 3 weeks! Was there an intentional meaning with speeding up this process, or was everything naturally moving so quickly at the time?</p>
+
+      </div>
+
+      <div className = "answer answer-1">
+
+      <p>SS: The original inspiration was from when NASA cancelled their all-female space mission on March 27, 2019. I was sitting in Starbucks in Ithaca, and I realized I wanted to create something around this. Originally, it was, “I’m gonna make a shirt!”. I already created the design — it’s changed since then — and I was very excited about it. And then came spring break. When I came back we had three weeks of school until everyone left campus. I wanted to launch it on campus, so that’s why it was in a very short time frame.</p>
+
+      </div>
+
+      </div>
+
+      <div className = "panel" id = "section2">
+
+      <div className = "blur">
+
+      <img src = "images/question2.JPG" alt = "pop up launch collection" />
+
+      </div>
+
+      <div className = "question question-2">
+
+      <p>JC: Did you feel like it was rushed? Did you ever wish that maybe you had had more time with launching?</p>
+
+      </div>
+
+      <div className = "answer answer-2">
+
+      <p>SS: I think that it was what I wanted in a way. I launched it in a pop-up store design in Collegetown. I wanted the community to be as involved in it as possible. Going up to it [the launch], no one knew it was a clothing line until one week before, and when they came, that’s when they saw the designs for the first time, so I just like bringing people to a physical space with it. </p>
+
+      </div>
+
+      </div>
+
+      <div className = "panel" id = "section3">
+
+      <div className = "blur">
+
+      <img src = "images/question3.png" alt = "the full THIS IS MY SPACESUIT collection"/>
+
+      </div>
+
+      <div className = "question question-3">
+
+      <p>JC: Was SPACESUIT always a passion project for you and something serious? Or did it grow from a hobby?</p>
+
+      </div>
+
+      <div className = "answer answer-3">
+
+      <p>SS: Yea, I was always very serious about this. Even in the language, you can tell from the very early marketing launch as a fashion brand, there were very intentional things behind the language that set it up for the future. There were only two items when it launched, but it was launched as a collection, and there was just a lot of intention behind it.</p>
+
+      </div>
+
+      </div>
+
+      <div className = "blockquote" id = "quote1">
+      <h3>"It’s really important to have your mission streamline everything that you do."</h3>
+      </div>
+
+
+      <div className = "panel" id = "section4">
+
+      <div className = "blur">
+
+      <img src = "images/question4.jpeg" alt = "workshop in Milan" />
+
+      </div>
+
+      <div className = "question question-4">
+
+      <p>JC: When you started it, did you see it expanding into not just a brand, but also an empowering movement and series of academic workshops? </p>
+
+      </div>
+
+      <div className = "answer answer-4">
+
+      <p>SS: No, no! No, not that part. But I guess, in general, I thought of it being very large — not large like bringing in profit, but being meaningful. That was the core. It was very rooted in the mission, which is why I think it set it up for the future. So people who are looking to create these businesses or want to start on that idea, I think it’s really important to have your mission streamline everything that you do. You shouldn’t really be creating businesses  for the sake of it or just for profit when you don’t even know if it solves a problem or if it helps anyone.</p>
+
+      </div>
+
+      </div>
+
+      <div className = "panel" id = "section5">
+
+      <div className = "blur">
+
+      <img src = "images/question5.png" alt = "collage of everything THIS IS MY SPACESUIT has done so far!" />
+
+      </div>
+
+      <div className = "question question-5">
+
+      <p>JC: In what way has SPACESUIT been the most life changing to you?</p>
+
+      </div>
+
+      <div className = "answer answer-5">
+
+      <p>SS: It’s taught me even more that you can do anything that you put your soul into. It’s really helped me spread this mission around the world and my internal mission too: to empower anyone to be a creator, that anyone can do what they want to do with their life, so that’s where anyone can blast off really stems from. It’s something that I strongly believe in.</p>
+
+      </div>
+
+      </div>
+
+      <div className = "panel" id = "section6">
+
+      <div className = "blur">
+
+      <img src = "images/question6.jpeg" alt = "embroidery workshop" />
+
+      </div>
+
+      <div className = "question question-6">
+
+      <p>JC: In response to the whole COVID-19 situation, when did you first realize you would have to adapt SPACESUIT’s plans to the new quarantine lifestyle?</p>
+
+      </div>
+
+      <div className = "answer answer-6">
+
+      <p>SS: It was the last wearable technology workshop that was taught [this semester], and I was expressing to people that this might be the last one because I knew of the events that were going on. But I knew it wouldn’t be an ending, I just knew it would be an end for that physical location at the time. I always knew that the whole company would adapt to virtual workshops and that it was definitely possible, but it takes a different mindset to figure out how to make a one hour workshop helpful for anyone around the world now. In the end, when things were cut short, we had already gotten to do so much and I had gotten to meet and bring so many people together, that it was okay. I knew that this chapter could close here.</p>
+
+      </div>
+
+      </div>
+
+      <div className = "blockquote" id = "quote2">
+        <h3>"You’re like, 'Oh, I have to deal with this first' or 'I need to be at my high when I talk to someone' — but just call. Get the conversation flowing."</h3>
+      </div>
+
+      <div className = "panel" id = "section7">
+
+      <div className = "blur">
+
+      <img src = "images/question7.JPG" alt = "Shoshana's quarantine look" />
+
+      </div>
+
+      <div className = "question question-7">
+
+      <p>JC: What advice would you have for students who are trying to maintain being inspired and creative during this pandemic?</p>
+
+      </div>
+
+      <div className = "answer answer-7">
+
+      <p>SS: Try doing a new activity every morning. Oh, there’s a lot of things. Oh my god. Just anything about inspiration, this is like my go-to — I could write a novel about this. Well, first you have to focus on your health and well-being. I’m a big <a href="headspace.com" id="headspace">Headspace</a> user. I start every morning and go to bed with Andy tucking me in — he’s the guy who speaks. So that really helps me wake up with a clear head. Another thing I try doing in the morning is a journaling exercise where you write down your daily intentions, your daily goals, and things you’re grateful for. A very quick exercise, but I think it’s a great thing to look back on. Additionally, I get a lot of inspiration by talking to others. I’m lucky I have so many creative and cool friends. I call them and am just like “What are you doing? What are you up to?” A basic conversation with them, not necessarily about projects, helps me break out of my own head. During this time, if you’re at one of your low points or you don’t feel great about your day, you don’t really think about reaching out to people. You’re like, “Oh, I have to deal with this first” or “I need to be at my high when I talk to someone” — but just call. Get the conversation flowing.</p>
+
+      </div>
+
+      </div>
+
+      <div className = "panel" id = "lastsection">
+
+      <div className = "question question-8">
+
+      <p>JC: As for you, how has quarantine affected your creativity personally with all this new free time?</p>
+
+      </div>
+
+      <div className = "answer answer-8">
+
+      <p>SS: I have a new definition of ‘busy’ these days. So when I express to my friends, “Oh, I’ve been busy”, it’s in a good way. I’ve really just tried to focus my energy towards things that could help this time and the people in this time. A lot of that is through pouring my energy into the workshops and the products that are coming out because I think it could really benefit people and empower them to still continue to create. </p>
+
+      </div>
+
+      </div>
+
+      <div id = "ending">
+      <p id = "tagline">Anyone can blast off.</p>
+
+      <p>Check out <a target="_blank" href="http://thisismyspacesuit.com/">http://thisismyspacesuit.com/</a>.</p>
+      <p>Follow <a target="_blank" href="https://www.instagram.com/thisismyspacesuit/">@thisismyspacesuit</a> + <a target="_blank" href="https://www.instagram.com/shoshanaswell/">@shoshanaswell</a> to stay up to date on future missions.</p>
+
+      </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
